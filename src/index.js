5b249e629915c314d990f6994c12361d089731e7
Sayush Kamat <50794723+thedarkdivinity@users.noreply.github.com>
2020-05-18 15:42:01 +0530

    Init commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import App from "./App";
-
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
+  <>
+    <div className="cards">
+      <div className="card">
+        <img
+          src="https://wallpapercave.com/wp/wp4056410.jpg"
+          alt="myPic"
+          className="card__img"
+        />
+        <div className="card__info">
+          <span className="card__category">A Netflix Original Series</span>
+          <h3 className="card__title">DARK</h3>
+          <a
+            href="https://www.netflix.com/in/title/80990668?source=35"
+            target="_blank"
+          >
+            <button>Watch Now</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </>,
+  document.getElementById("root")
 );
