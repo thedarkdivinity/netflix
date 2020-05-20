261f357e11d8e27c7102fa65eda6267c7114e130
Sayush Kamat <50794723+thedarkdivinity@users.noreply.github.com>
2020-05-20 15:51:23 +0530

    Our Netflix App is ready
    To add more series , modify sdata.js

--- src/index.js
+++ src/index.js
@@ -2,26 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./Card";
 import "./styles.css";
+import sdata from "./sdata";
+function ncard(val) {
+  return (
+    <Card
+      imgsrc={val.imgsrc}
+      title={val.title}
+      sname={val.sname}
+      link={val.link}
+    />
+  );
+}
+
 ReactDOM.render(
   <>
-    <Card
-      imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
-      title="A Netflix Original Series"
-      sname="DARK"
-      link="https://www.netflix.com/in/title/80100172"
-    />
-    <Card
-      imgsrc="https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABcYWogkUru45KRDP62tg_ZvlKu-2H5XtNrPdhRN5nSLOBBPCR032FLTAAsOwlv_tUi6p_0vPVd8KdtYaZFZ_z6mYGkXZ.jpg?r=8bf"
-      title="A Netflix Original Series"
-      sname="DARK"
-      link="https://www.netflix.com/in/title/80990668?source=35"
-    />
-    <Card
-      imgsrc="https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABSoEEFwl0i4fmFQmSDQOZ_OrshmrVV7tbLEw9X7mLRBtmyyJ5Hkw1Fda2WG-AqIBQ6hEF4sqUEnT_VLeBCpVRcAhiwm-.jpg?r=a42%22"
-      title="A Netflix Original Series"
-      sname="STRANGER THINGS"
-      link="https://www.netflix.com/in/title/80057281"
-    />
+    <h1 className="heading">List of top 5 netflix series in 2020</h1>
+    {sdata.map(ncard)}
   </>,
   document.getElementById("root")
 );
---
+++ src/sdata.js
@@ -0,0 +1,30 @@
+const sdata = [
+  {
+    sname: "DARK",
+    imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80100172"
+  },
+  {
+    sname: "EXTRA CURRICULAR",
+    imgsrc:
+      "https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABcYWogkUru45KRDP62tg_ZvlKu-2H5XtNrPdhRN5nSLOBBPCR032FLTAAsOwlv_tUi6p_0vPVd8KdtYaZFZ_z6mYGkXZ.jpg?r=8bf",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80990668?source=35"
+  },
+  {
+    sname: "STRANGER THINGS",
+    imgsrc:
+      "https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABSoEEFwl0i4fmFQmSDQOZ_OrshmrVV7tbLEw9X7mLRBtmyyJ5Hkw1Fda2WG-AqIBQ6hEF4sqUEnT_VLeBCpVRcAhiwm-.jpg?r=a42%22",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80057281"
+  },
+  {
+    sname: "The Vampire Diaries",
+    imgsrc: "https://images.justwatch.com/poster/8589251/s592",
+    title: "An Amazon Original Series",
+    link:
+      "https://www.primevideo.com/detail/amzn1.dv.gti.26ac3a30-e117-04f7-ca76-546cf287e45a/?ref_=dvm_pds_gen_in_as_s_gt_dtites214E4|m_L5ZEDwBfc_c413303891014_s"
+  }
+];
+export default sdata;
